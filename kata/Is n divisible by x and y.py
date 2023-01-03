3eb88b9c1003febeeeda9e236bb666e7fe88efa5
thatguy-lcgm <116634183+thatguy-lcgm@users.noreply.github.com>
2023-01-03 05:49:42 -0300

    Update Is n divisible by x and y.py

--- kata/Is n divisible by x and y.py
+++ kata/Is n divisible by x and y.py
@@ -1,2 +1,6 @@
+"""Create a function that checks if a number n is divisible by
+two numbers x AND y. All inputs are positive, non-zero numbers."""
+
+
 def is_divisible(n, x, y):
     return True if n % x == 0 and n % y == 0 else False
