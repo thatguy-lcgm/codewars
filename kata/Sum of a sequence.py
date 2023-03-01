380fb2bb13f2e18bbf8009d4fa277d1e76d2b3e7
thatguy-lcgm <116634183+thatguy-lcgm@users.noreply.github.com>
2023-03-01 14:58:44 -0300

    Create Sum of a sequence.py

---
+++ kata/Sum of a sequence.py
@@ -0,0 +1,21 @@
+"""
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+This is the first kata in the series:
+
+Sum of a sequence (this kata)
+https://www.codewars.com/kata/586f6741c66d18c22800010a/train/python
+"""
+
+def sequence_sum(b, e, s):
+    return sum([x for x in range(b, e + 1, s)])
